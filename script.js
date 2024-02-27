0ddaedfbccc5cc1985cee8cb37e001f0cf13172b
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-02-26 22:00:24 -0300

    função calculadora diarias

--- script.js
+++ script.js
@@ -2,18 +2,37 @@
 function calcularValorPorArroba() {
     // Obter valores dos campos
     const totalVenda = parseFloat(document.getElementById('totalVenda').value);
-    const pesoBoi = parseFloat(document.getElementById('pesoBoi').value);
+    const pesoBoiRecebido = parseFloat(document.getElementById('pesoBoi').value);
+    const pesoBoiVendido = parseFloat(document.getElementById('pesoBoiVendido').value);
     const unidadePeso = document.getElementById('unidadePeso').value;
 
     // Converter o peso para arrobas, se necessário
-    const pesoEmArrobas = (unidadePeso === 'kg') ? pesoBoi / 15 : pesoBoi;
+    const pesoEmArrobasEsperado = (unidadePeso === 'kg') ? pesoBoiRecebido / 15 : pesoBoiRecebido;
 
-    // Calcular valor por arroba
-    const valorPorArroba = totalVenda / pesoEmArrobas;
+    // Converter o peso para arrobas, se necessário
+    const pesoEmArrobasVendido = (unidadePeso === 'kg') ? pesoBoiVendido / 15 : pesoBoiVendido;
 
-    // Exibir o resultado na página
-    const resultadoDiv = document.getElementById('resultado');
-    resultadoDiv.innerHTML = `<p>Valor por Arroba: R$ ${valorPorArroba.toFixed(2)}</p>`;
+    // Calcular valor por arroba Esperada na Venda
+    const valorPorArrobaEsperada = totalVenda / pesoEmArrobasEsperado;
+
+    // Calcular valor por arroba Esperada na Venda
+    const valorPorArrobaVendido = totalVenda / pesoEmArrobasVendido;
+
+    // Calcular valor por arroba Esperada na Venda
+    const diferecaSobrou = (pesoBoiRecebido) - (pesoBoiVendido);
+
+
+    // Exibir o resultado na página valor esperado
+    const resultadoDivEsperado = document.getElementById('resultadoEsperado');
+    resultadoDivEsperado.innerHTML = `<p>Valor por Arroba Esperado: R$ ${valorPorArrobaEsperada.toFixed(2)}</p>`;
+
+    // Exibir o resultado na página valor vendido
+    const resultadoDivVendido = document.getElementById('resultadoReal');
+    resultadoDivVendido.innerHTML = `<p>Valor por Arroba Vendido: R$ ${valorPorArrobaVendido.toFixed(2)}</p>`;
+
+    // Exibir o resultado na página diferença do peso recebido e vendido
+    const diferencaPeso = document.getElementById('sobrou');
+    diferencaPeso.innerHTML = `<p>Quantidade não Vendida: ${diferecaSobrou.toFixed(2)} </p>`;
 }
 
 // script.js
@@ -23,11 +42,31 @@ $(document).ready(function() {
     // Função para limpar os valores
     function limparValores() {
         $('#totalVenda').val('');
-        $('#pesoBoi').val('');
+        $('#pesoBoiRecebido').val('');
         $('#unidadePeso').val('arroba'); // Defina a unidade de peso padrão conforme necessário
+        $('#resultado').empty();
+        $('#pesoBoiVendido').val('');
+        $('#unidadePesoVendido').val('arroba'); // Defina a unidade de peso padrão conforme necessário
         $('#resultado').empty();
     }
 
     // Associar a função ao botão de limpar
     $('#limparBtn').on('click', limparValores);
 });
+
+$(document).ready(function(){
+    // Aplica a máscara de dinheiro ao campo de entrada
+    $('#totalVenda').inputmask('currency', {
+      alias: 'numeric',
+      radixPoint: ',',
+      groupSeparator: '.',
+      autoGroup: true,
+      digits: 2,
+      digitsOptional: false,
+      prefix: 'R$ ',
+      placeholder: '0',
+      rightAlign: false,
+      autoUnmask: true,
+      removeMaskOnSubmit: true
+    });
+  });
